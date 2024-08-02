a45e7009b626dab8a56df453407d7b8f3e317651
Nair3091 <dhruv14nair@gmail.com>
2024-08-02 12:08:30 +0530

    django cimmit

--- calc/views.py
+++ calc/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render 
+from .models import *
 def home(request):
     return render(request,'home.html',{'name':'Raju'})
 def add(request):
@@ -7,4 +8,10 @@ def add(request):
     val3=val1+val2
     return render(request, "result.html",{"result":val3})
 def dashboard(request): 
-    return render(request,'dashboard.html') 
+    return render(request,'dashboard.html')
+def dashboard(request):
+    customers=Customer.objects.all()
+    return render(request,"dashboard.html",{"customers":customers})
+def products(request):
+    products= Product.objects.all() 
+    return render(request ,'products.html',{'products':products})
